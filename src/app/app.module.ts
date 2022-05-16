87606661ef5c1eea8b4c33bfae4ada8415ff957a
belublu <94806268+belublu@users.noreply.github.com>
2022-05-16 13:40:38 -0300

    Creación de botón

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { LogoArgPComponent } from './components/logo-arg-p/logo-arg-p.component';
 import { RedesComponent } from './components/redes/redes.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { BannerComponent } from './components/banner/banner.component';
     HeaderComponent,
     LogoArgPComponent,
     RedesComponent,
-    BannerComponent
+    BannerComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
